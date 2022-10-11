583d3003a1f22677f640f602d81e7385bbc9ad10
ICS4U-1-2022 <113033471+ICS4U-1-2022@users.noreply.github.com>
2022-10-11 09:21:01 -0400

    use new LSP code

---
+++ .config/nvim/lua/user/lsp_old/lsp.lua
@@ -0,0 +1,10 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+	return
+end
+
+-- install new LSP setup Mason
+require("mason").setup()
+require("mason-lspconfig").setup({
+    ensure_installed = { "sumneko_lua", "pyright", "jdtls", "tsserver" }
+})
