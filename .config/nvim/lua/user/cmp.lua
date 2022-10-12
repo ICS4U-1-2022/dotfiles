ff39d3dea6bce2a134174286a763213409603e58
ICS4U-1-2022 <113033471+ICS4U-1-2022@users.noreply.github.com>
2022-10-12 09:47:24 -0400

    remove mason

--- .config/nvim/lua/user/cmp.lua
+++ .config/nvim/lua/user/cmp.lua
@@ -102,7 +102,6 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
-        mason = "[Mason-LSP"],
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
@@ -112,7 +111,6 @@ cmp.setup {
   },
   sources = {
     { name = "nvim_lsp" },
-    { name = "mason" },
     { name = "buffer" },
     { name = "luasnip" },
     { name = "path" },
